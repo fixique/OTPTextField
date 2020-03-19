3ec7b6b98ca464f6a90290b8db19b8e7dc62d9d1
Krupenko Validislav <dev.fixique@gmail.com>
2020-03-19 13:29:10 +0300

    Configure project

---
+++ OTPTextFieldExample/OTPTextFieldExample/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  OTPTextFieldExample
+//
+//  Created by Vladislav Krupenko on 19.03.2020.
+//  Copyright © 2020 Fixique. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
---
+++ OTPTextFieldExample/OTPTextFieldExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OTPTextFieldExample
+//
+//  Created by Vladislav Krupenko on 19.03.2020.
+//  Copyright © 2020 Fixique. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
